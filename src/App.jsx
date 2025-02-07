24f5f7811fffed95d3c32ce8660b004c27fc323b
Alecio <aleciosilveira97@gmail.com>
2025-02-07 00:36:56 -0300

    Excluindo itens do banco de dados

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { db } from './firebaseConnection'
-import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc } from 'firebase/firestore'
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc } from 'firebase/firestore'
 import './app.css'
 
 
@@ -95,6 +95,15 @@ async function buscarPost() {
 
   }
 
+ async function excluirPost(id){
+   const docRef = doc(db, "posts", id)
+   await deleteDoc(docRef)
+   .then(() =>{
+    alert("Post excluido com sucesso")
+   })
+
+  }
+
   return (
     <div>
       <h1>App fire</h1>
@@ -134,6 +143,7 @@ async function buscarPost() {
                 <strong>ID: {post.id}</strong> <br/>
                 <span>Titulo: {post.titulo} </span> <br/>
                 <span>Autor: {post.autor}</span> <br/>
+                <button onClick={()=> excluirPost (post.id) }>Excluir</button> <br/> <br/>
               </li>
             )
           })}
